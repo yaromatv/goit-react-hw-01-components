eda950d5ab111e6e99095702488c42f7fdf2624b
yaromatv <122043732+yaromatv@users.noreply.github.com>
2023-07-20 20:36:01 +0200

    1st task done

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,42 @@
+import css from 'components/Profile/Profile.module.css';
+import { formatNumberWithCommas } from 'utils/formatNumberWithCommas';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  followers,
+  views,
+  likes,
+}) => {
+  const formattedFollowers = formatNumberWithCommas(followers);
+  const formattedViews = formatNumberWithCommas(views);
+  const formattedLikes = formatNumberWithCommas(likes);
+
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{formattedFollowers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{formattedViews}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{formattedLikes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
